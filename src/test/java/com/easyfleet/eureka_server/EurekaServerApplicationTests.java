e485610ea35b624ce797f6bb44e03ed2a949fbe6
Nikolay <kalmykov.n.s.89@gmail.com>
2024-10-27 10:26:39 +0100

    [#-0] Eureka server config

--- src/test/java/com/easyfleet/eureka_server/EurekaServerApplicationTests.java
+++ src/test/java/com/easyfleet/eureka_server/EurekaServerApplicationTests.java
@@ -1,8 +1,10 @@
 package com.easyfleet.eureka_server;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class EurekaServerApplicationTests {
 
